22f6bcf8ea2be896179512bf06e320210ca49893
Hebi Li <lihebi.com@gmail.com>
2019-10-24 14:17:25 -0500

    cleanup docfile

--- docker/startvnc.sh
+++ docker/startvnc.sh
@@ -8,11 +8,11 @@ echo "IP Address of this container: " `hostname -i`
 echo "Starting openbox session on :5901 .."
 vncserver :1 -localhost no -xstartup $HOME/.vnc/xstartup
 
-echo "Starting xmonad session on :5902 .."
-vncserver :2 -localhost no -xstartup $HOME/.vnc/xstartup-xmonad
+# echo "Starting xmonad session on :5902 .."
+# vncserver :2 -localhost no -xstartup $HOME/.vnc/xstartup-xmonad
 
-echo "Starting stumpwm session on :5903 .."
-vncserver :3 -localhost no -xstartup $HOME/.vnc/xstartup-stumpwm
+# echo "Starting stumpwm session on :5903 .."
+# vncserver :3 -localhost no -xstartup $HOME/.vnc/xstartup-stumpwm
 
 echo "Done"
 
