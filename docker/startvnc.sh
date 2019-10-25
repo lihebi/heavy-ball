f3921bfc6fced5e6371dd5ea3cdbaa0eba3c45b7
Hebi Li <lihebi.com@gmail.com>
2019-10-25 10:46:21 -0500

    fix LXC problems

--- docker/startvnc.sh
+++ docker/startvnc.sh
@@ -5,9 +5,17 @@
 #
 echo "IP Address of this container: " `hostname -i`
 
-echo "Starting lxd service"
-echo "hebi" | sudo --stdin service lxd start
-service lxd status
+# echo "Starting lxd service .."
+# echo "hebi" | sudo --stdin service lxd start
+# service lxd status
+
+echo "Starting ssh server .."
+echo "hebi" | sudo --stdin service ssh start
+service ssh status
+
+# TODO test if :1 is taken
+echo "Killing existing :1 vnc server .."
+vncserver -kill :1
 
 echo "Starting openbox session on :5901 .."
 vncserver :1 -localhost no -xstartup $HOME/.vnc/xstartup
