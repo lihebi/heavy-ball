db560c61ced606d42f693231cfadaf66cafd089e
Hebi Li <lihebi.com@gmail.com>
2019-10-26 19:32:05 -0500

    add gpds and routing templates

--- src/generate-xml.sh
+++ src/generate-xml.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 nodecount=4
+
+olsd_name=$(find /usr/lib* -name "olsrd_txtinfo.so*" -print 2> /dev/null)
+olsd_name=$(basename $olsd_name)
+
 for name in templates/*; do
     name=$(basename $name)
     for nodeid in $(seq 1 $nodecount); do
-        echo "Generating xml/${name%%.*}$nodeid.xml"
-        sed -e 's|@NEMID[@]|$nodeid|g' \
-            -e 's|@NODEID[@]|$nodeid|g' \
-            templates/$name > xml/${name%%.*}$nodeid.xml
+        name1=${name/.template/}
+        name2=${name1%.*}$nodeid.${name1#*.}
+        echo "Generating xml/$name2"
+        sed -e "s|@NEMID[@]|$nodeid|g" \
+            -e "s|@NODEID[@]|$nodeid|g" \
+            -e "s|@OLSRTXTINFO[@]|$olsd_name|g" \
+            templates/$name > xml/$name2
     done
 done
-
-
